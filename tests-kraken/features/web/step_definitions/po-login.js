ce44347326fe8ef262e8b6aa12d2e2ada092b8c2
StevenRey1 <js.reyh1@uniandes.edu.co>
2024-05-03 02:22:11 -0500

    Feature modificar Post y crear Post

--- tests-kraken/features/web/step_definitions/po-login.js
+++ tests-kraken/features/web/step_definitions/po-login.js
@@ -1,12 +1,11 @@
 async function login(context, email, password){
-    let element = await context.driver.$('#identification');
+    let element = await context.driver.$('#ember6');
     await element.setValue(email);
-    element = await context.driver.$('#password');
+    element = await context.driver.$('#ember8');
     await element.setValue(password);
-    element = await context.driver.$('#ember5');    
+    element = await context.driver.$('#ember10');
     return await element.click();
 }
-
 module.exports = {
     login
 };
--- tests-kraken/features/web/step_definitions/step.js
+++ tests-kraken/features/web/step_definitions/step.js
@@ -27,3 +27,99 @@ Then('I see the text {string}', async function (error) {
 Given('I login {kraken-string} {kraken-string}', async function (email, password) {
     login(this, email, password);
 });
+
+/* FUNCIONALIDAD 2 CREATE POST*/
+
+When('I clic in back button', async function () {
+    let element = await this.driver.$('a[class="ember-view gh-btn-editor gh-editor-back-button"]');
+    return await element.click();
+});
+
+
+Then('I see the first post in list with name untitled', async function () {
+    let element =await this.driver.$('li.gh-list-row.gh-posts-list-item a.gh-list-data.gh-post-list-title');
+    let textElement= await element.getText();
+    assert(textElement.includes("(Untitled)"));
+});
+
+
+When('I enter title longer', async function () {
+    let element = await this.driver.$('.gh-editor-title.ember-text-area.gh-input.ember-view');
+    const titleLarge = 'a'.repeat(256); // Genera un título de 256 caracteres
+    return await element.setValue(titleLarge); // Establece el título largo
+    
+});
+
+Then('I hope the publish button is not shown', async function () {
+    let element =await this.driver.$('gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger');
+    let isVisibleElement= await element.isDisplayed();
+    assert.ok(!isVisibleElement);
+});
+
+Then('I see the first post in list with name Normal Title', async function () {
+    let element =await this.driver.$('li.gh-list-row.gh-posts-list-item a.gh-list-data.gh-post-list-title');
+    let textElement= await element.getText();
+    assert(textElement.includes("Normal Title"));
+});
+
+Then('I confirm that the title page contains the post title {string}', async function (newTitle) {
+    const title = await this.driver.getTitle();
+    assert(title.includes(newTitle));
+});
+
+When('I click in select tag', async function () {
+    const element= await this.driver.$(".ember-power-select-trigger-multiple-input")
+    await element.click();
+});
+
+When('I clic in the tag news', async function () {
+    const listItems = await this.driver.$('li[data-option-index="0"]'); // Seleccionar todos los <li>
+    // Hacer clic en el elemento
+    return await listItems.click();
+});
+
+
+
+Then('I see the tag on the page {string}', async function (newTag) {
+    const element = await this.driver.$(".post-card-primary-tag");
+    let textElement= await element.getText();
+    assert(textElement.includes(newTag));
+});
+
+/* FUNCIONALIDAD MODIFY POST*/
+
+Then('I see the error {string}', async function (error) {
+    const element = await this.driver.$(".response");
+    let textElement= await element.getText();
+    assert(textElement.includes(error));
+});
+
+When('I enter new title {string}', async function (newTitle) {
+    const element= await this.driver.$('.gh-editor-title.ember-text-area.gh-input.ember-view');
+    return await element.setValue(newTitle);
+    
+});
+
+Then('I see the text of the page {string}', async function (title) {
+    const element = await this.driver.$(".article-title");
+    let textElement= await element.getText();
+    assert(textElement.includes(title));
+});
+
+When('I enter new url {string}', async function (newUrl) {
+    const element= await this.driver.$('#url');
+    return await element.setValue(newUrl);
+    
+});
+
+Then('I see the error about longer title {string}', async function (title) {
+    const element = await this.driver.$(".gh-alert-content");
+    let textElement= await element.getText();
+    console.log(textElement);
+    assert(textElement.includes(title));
+});
+
+
+
+
+
