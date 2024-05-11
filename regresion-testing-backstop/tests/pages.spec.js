7ec14bbbfdc509ee7bc165870923148732cfb2ce
Jairo Reyes <ja.reyesr1@uniandes.edu.co>
2024-05-10 21:52:43 -0500

    Se ajusta test para que sea iterativo por cada aplicación bajo pruebas

--- regresion-testing-backstop/tests/pages.spec.js
+++ regresion-testing-backstop/tests/pages.spec.js
@@ -7,25 +7,31 @@ const projectConfig = require("../project-config.json");
 test("create no author page", async ({ page }) => {
   const scenarioTag = "page-no-author";
 
-  await page.goto(projectConfig.appsUnderTests[0].baseUrl + "/#/signin");
-  const loginPage = new LoginPage(
-    page,
-    projectConfig.appsUnderTests[0].screenshotsPath,
-    scenarioTag
-  );
-  await loginPage.submitLoginForm(
-    projectConfig.credentials.email,
-    projectConfig.credentials.password
-  );
-  await expect(page).toHaveURL(
-    projectConfig.appsUnderTests[0].baseUrl + "/#/dashboard"
-  );
-  const pagesPage = new PagesPage(
-    page,
-    projectConfig.appsUnderTests[0].screenshotsPath,
-    scenarioTag
-  );
-  const author = await pagesPage.pageNoAuthor();
+  // Hace lo mismo par cada app bajo pruebas definida
+  const navigationPromises = projectConfig.appsUnderTests.map(async app => {
+     await page.goto(app.baseUrl + "/#/signin");
+    
+    const loginPage = new LoginPage(
+      page,
+      app.screenshotsPath,
+      scenarioTag
+    );
+    await loginPage.submitLoginForm(
+      projectConfig.credentials.email,
+      projectConfig.credentials.password
+    );
+    await expect(page).toHaveURL(
+      app.baseUrl + "/#/dashboard"
+    );
+    const pagesPage = new PagesPage(
+      page,
+      app.screenshotsPath,
+      scenarioTag
+    );
+    const author = await pagesPage.pageNoAuthor();
+  
+    expect(author).toBe("");
 
-  expect(author).toBe("");
+  });
+  await Promise.all(navigationPromises);
 });
