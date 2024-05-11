8c43c4e44ed430ed58cffd6f53f606d196de6238
FalofoAndes <f.lozanof@uniandes.edu.co>
2024-05-10 19:28:43 -0500

    se cambia browser de edge a Chrome

--- tests-kraken/features/web/support/hooks.js
+++ tests-kraken/features/web/support/hooks.js
@@ -2,7 +2,7 @@ const { After, Before } = require('@cucumber/cucumber');
 const { WebClient } = require('kraken-node');
 
 Before(async function() {
-  this.deviceClient = new WebClient('edge', {}, {"userId":this.userId, "headless":true} );
+  this.deviceClient = new WebClient('chrome', {}, {"userId":this.userId, "headless":false} );
   this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
 })
 
