7b8b2209e4d9388612f6dc5a313e5bca874d9ccb
Jairo Reyes <ja.reyesr1@uniandes.edu.co>
2024-05-10 19:33:12 -0500

    Se reacomoda las carpetas de backstop y regresion testing

---
+++ regresion-testing-backstop/tests/post.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/login-page";
+import { PostPage } from "../pages/post-page";
+
+const CASES = {
+  baseUrl: "https://ghost-ur1e.onrender.com/ghost/#",
+  screenshotsPath: "../screenshots/ghost-5.14.1",
+};
+
+test("testing creating new untitled post", async ({ page }) => {
+  await page.goto(CASES.baseUrl + "/signin");
+  const loginPage = new LoginPage(
+    page,
+    CASES.screenshotsPath,
+    "post-untitled-post"
+  );
+  await loginPage.submitLoginForm(
+    "pruebauniandes@uniandes.edu.co",
+    "Uniandes123456"
+  );
+  await expect(page).toHaveURL(CASES.baseUrl + "/dashboard");
+  const postPage = new PostPage(
+    page,
+    CASES.screenshotsPath,
+    "post-untitled-post"
+  );
+  const title = await postPage.fillPostUntitled(
+    "Nuevo untitle",
+    "Sería untitled"
+  );
+
+  // Se hace la asserción final 
+  expect(title).toBe("(Untitled)");
+});
+
+test("testing URL of new post", async ({ page }) => {
+  await page.goto(CASES.baseUrl + "/signin");
+  const loginPage = new LoginPage(
+    page,
+    CASES.screenshotsPath,
+    "post-url-new-post"
+  );
+  await loginPage.submitLoginForm(
+    "pruebauniandes@uniandes.edu.co",
+    "Uniandes123456"
+  );
+  await expect(page).toHaveURL(CASES.baseUrl + "/dashboard");
+  const postPage = new PostPage(page);
+  await postPage.accesingNewPost("Nuevos post con URL ", "Cuenta con URL");
+});
