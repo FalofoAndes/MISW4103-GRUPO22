cd4f4db9ebc27fdb43df10f19d9e9b0ccef4afb4
carlosrios <c.riosp@uniandes.edu.co>
2024-05-12 23:07:48 -0500

    Carga de reporte HTML BackStop

---
+++ regresion-testing-backstop/backstop/backstop_data/html_report/config.js
@@ -0,0 +1,58 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Inicio_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240508-230200\\backstop_default_Inicio_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Inicio_0_document_0_default.png",
+        "label": "Inicio",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "https://monitor177.github.io/color-palette/",
+        "referenceUrl": "https://monitor177.github.io/color-palette/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 5.960416666666666,
+          "misMatchPercentage": "5.96",
+          "analysisTime": 26
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Paleta_de_colores_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240508-230200\\backstop_default_Paleta_de_colores_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Paleta_de_colores_0_document_0_default.png",
+        "label": "Paleta de colores",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "https://carlosarios.github.io/color-palette/",
+        "referenceUrl": "https://monitor177.github.io/color-palette/",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 5.729166666666666,
+          "misMatchPercentage": "5.73",
+          "analysisTime": 23
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
